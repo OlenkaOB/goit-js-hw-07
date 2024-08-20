6face9d42a099ce1339b9fdae33b739394f60d05
olenka <elena.kaminnik@gmail.com>
2024-08-20 23:43:05 +0300

    add index file

--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,9 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const gallery = document.querySelector(".gallery");
+const newGallery = document.createElement("li");
+console.log(newGallery);
