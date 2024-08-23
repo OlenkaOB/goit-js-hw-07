39d405c1d97f7329459573dd73f3e051b953fce4
olenka <elena.kaminnik@gmail.com>
2024-08-23 23:12:53 +0300

    add task file

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,41 @@
+const controls = document.querySelector('#controls');
+const boxesContainer = document.querySelector('#boxes');
+const inputEl = controls.querySelector('input');
+const createBtn = controls.querySelector('[data-create]');
+const destroyBtn = controls.querySelector('[data-destroy]');
+
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(inputEl.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    inputEl.value = '';
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  boxesContainer.innerHTML = '';
+  const boxes = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    size += 10;
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
